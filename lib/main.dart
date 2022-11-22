a8592de7970b0630be0c063c2821905dc0aca52d
maenard <maenardaboabo065@gmail.com>
2022-11-22 19:53:44 +0800

    firebase set up done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:blog_app/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
