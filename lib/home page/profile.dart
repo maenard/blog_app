bfe190b1ba98aedc798082131d8dfa30b6057d45
maenard <maenardaboabo065@gmail.com>
2022-11-30 02:25:30 +0800

    PROFILE CHANGES

--- lib/home page/profile.dart
+++ lib/home page/profile.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/model/users.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,6 +12,7 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,17 +25,118 @@ class _ProfileState extends State<Profile> {
             fontWeight: FontWeight.bold,
           ),
         ),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            readCurrentUser(user.uid),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget userStats() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'POSTS',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                '150',
+                style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'STARS',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                '50',
+                style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
           ),
         ],
-      ),
-      body: Container(),
+      );
+
+  Widget userDetail(Users newUser) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/images/photo_male_7.jpg"),
+            radius: 50,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                newUser.name,
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                newUser.email,
+                style: GoogleFonts.poppins(
+                  color: Colors.white54,
+                  fontSize: 12,
+                  height: .8,
+                ),
+              ),
+              userStats(),
+            ],
+          ),
+        ],
+      );
+
+  Widget readCurrentUser(uid) {
+    var collection = FirebaseFirestore.instance.collection('users');
+    return Column(
+      children: [
+        StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          stream: collection.doc(uid).snapshots(),
+          builder: (_, snapshot) {
+            if (snapshot.hasError) return Text('Error = ${snapshot.error}');
+
+            if (snapshot.hasData) {
+              final users = snapshot.data!.data();
+              final newUser = Users(
+                id: users!['id'],
+                name: users['name'],
+                password: users['password'],
+                email: users['email'],
+              );
+
+              return (userDetail(newUser));
+            }
+
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
+      ],
     );
   }
 }
