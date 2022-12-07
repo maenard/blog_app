3121dfaab7e2e50a1529e3743b19f9e268ac6150
maenard <maenardaboabo065@gmail.com>
2022-12-07 15:27:22 +0800

    splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,21 @@ import 'package:blog_app/login/verifyEmail.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(initialization);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds: 5));
 }
 
 final navigatorKey = GlobalKey<NavigatorState>();
